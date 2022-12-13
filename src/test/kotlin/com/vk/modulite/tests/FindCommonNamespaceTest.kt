e95f1f80edfec843145a63737c8058929cd274fe
Danil Ovchinnikov <danil42worker@gmail.com>
2022-12-13 19:18:10 +0300

    removed kotest from the dependencies (#6)

--- src/test/kotlin/com/vk/modulite/tests/FindCommonNamespaceTest.kt
+++ src/test/kotlin/com/vk/modulite/tests/FindCommonNamespaceTest.kt
@@ -3,20 +3,23 @@ package com.vk.modulite.tests
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.vk.modulite.Namespace
 import com.vk.modulite.services.ModuliteDependenciesCollector
-import io.kotest.matchers.shouldBe
 
 class FindCommonNamespaceTest : BasePlatformTestCase() {
     fun `test find common namespace`() {
-        common("A", "B")                shouldBe "<global namespace>"
-        common("A", "A")                shouldBe "A"
-        common("A", "A\\B")             shouldBe "A"
-        common("A\\B\\C", "A\\B\\D")    shouldBe "A\\B"
-        common("A\\B\\C", "A\\B\\C")    shouldBe "A\\B\\C"
-        common("A\\B\\C", "A\\B\\C\\D") shouldBe "A\\B\\C"
+        common("A", "B")                beEqual "<global namespace>"
+        common("A", "A")                beEqual "A"
+        common("A", "A\\B")             beEqual "A"
+        common("A\\B\\C", "A\\B\\D")    beEqual "A\\B"
+        common("A\\B\\C", "A\\B\\C")    beEqual "A\\B\\C"
+        common("A\\B\\C", "A\\B\\C\\D") beEqual "A\\B\\C"
     }
 
     private fun common(vararg namespaces: String): String {
         val nss = namespaces.toList().map { Namespace(it) }
         return ModuliteDependenciesCollector.getInstance(project).findCommonNamespace(nss).toPHP()
+    }
+
+    private infix fun <T, U: T> T.beEqual(expect: U?) {
+        assertEquals(expect, this)
     }
 }
