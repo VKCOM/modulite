45a9fad5bfeeeb78b64c943d29acc74ae7bc051f
Danil Ovchinnikov <danil42worker@gmail.com>
2023-02-14 23:10:44 +0300

    the correct resolve for global variables

--- src/main/kotlin/com/vk/modulite/services/ModuliteDependenciesCollector.kt
+++ src/main/kotlin/com/vk/modulite/services/ModuliteDependenciesCollector.kt
@@ -22,6 +22,7 @@ import com.vk.modulite.modulite.Modulite
 import com.vk.modulite.notifications.ModuliteWarningNotification
 import com.vk.modulite.psi.PhpRecursiveElementVisitor
 import com.vk.modulite.psi.extensions.files.*
+import com.vk.modulite.psi.extensions.php.resolveElement
 import com.vk.modulite.psi.extensions.php.safeFqn
 import com.vk.modulite.psi.extensions.php.symbolName
 import com.vk.modulite.utils.fromKphpPolyfills
@@ -282,7 +283,7 @@ class ModuliteDependenciesCollector(val project: Project) {
                 private fun handleReference(reference: PhpReference?, traverseFurther: Boolean = true) {
                     if (reference == null) return
 
-                    val references = reference.resolveGlobal(false)
+                    val references = reference.resolveElement()
                     if (references.isEmpty()) {
                         LOG.warn("Unknown reference '${reference.safeFqn()}'")
                         return
