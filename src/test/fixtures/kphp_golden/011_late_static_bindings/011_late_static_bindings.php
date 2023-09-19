1a2d4dbfb857e8a5fc3b135b04cc8657fb72a3eb
Richardas Kuchinskas <r.kuchinskas@vk.team>
2023-09-19 19:54:01 +0300

    Added new cases for tests

---
+++ src/test/fixtures/kphp_golden/011_late_static_bindings/011_late_static_bindings.php
@@ -0,0 +1,9 @@
+@ok
+KPHP_ENABLE_MODULITE=1
+<?php
+#ifndef KPHP
+require_once 'kphp_tester_include.php';
+#endif
+
+$_ = \Messages011\MessagesLogger011::log();
+\Messages011\MessagesLogger011::create();
