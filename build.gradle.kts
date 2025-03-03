c5721514a084650e9a55d2e056b4b32abd460550
Richardas Kuchinskas <r.kuchinskas@vk.team>
2025-03-03 16:42:14 +0300

    review fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -107,24 +107,3 @@ tasks {
     }
 
 }
-
-intellijPlatformTesting {
-    runIde {
-        register("runIdeForUiTests") {
-            task {
-                jvmArgumentProviders += CommandLineArgumentProvider {
-                    listOf(
-                        "-Drobot-server.port=8082",
-                        "-Dide.mac.message.dialogs.as.sheets=false",
-                        "-Djb.privacy.policy.text=<!--999.999-->",
-                        "-Djb.consents.confirmation.enabled=false",
-                    )
-                }
-            }
-
-            plugins {
-                robotServerPlugin()
-            }
-        }
-    }
-}
